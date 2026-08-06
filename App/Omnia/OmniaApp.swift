cc9e6f051a391f7a5b3bda7f4634bd4e2a07821c
MMBurobin <michael.burobin@yandex.ru>
2026-08-06 14:44:40 +0300

    feat: add Xcode workspace and app targets (RE-1) (#133)

---
+++ App/Omnia/OmniaApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import OmniaApp
+import OmniaPresentation
+
+@main
+struct OmniaMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Omnia")
+        }
+    }
+}
---
+++ App/OmniaiOS/OmniaiOSApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import OmniaApp
+import OmniaPresentation
+
+@main
+struct OmniaiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Omnia")
+        }
+    }
+}
